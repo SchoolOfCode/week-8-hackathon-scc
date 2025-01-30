c7cfb6175fa4e3395fa66b11785a17cd8766e1ec
Conner Adamsons <185359332+ConnerConner96@users.noreply.github.com>
2025-01-30 18:54:54 +0000

    added extra buttons on header to search for more games

--- hackathon-project/src/components/header/header.jsx
+++ hackathon-project/src/components/header/header.jsx
@@ -10,9 +10,17 @@ export default function Header({ onFilter }) {
       {/* Genre Filter Buttons */}
       <div className="filter-buttons">
         <button onClick={() => onFilter("Action")}>Action</button>
-        <button onClick={() => onFilter("FPS")}>FPS</button>
-        <button onClick={() => onFilter("RPG")}>RPG</button>
+        <button onClick={() => onFilter("Indie")}>Indie</button>
         <button onClick={() => onFilter("Adventure")}>Adventure</button>
+        <button onClick={() => onFilter("RPG")}>RPG</button>
+        <button onClick={() => onFilter("Strategy")}>Strategy</button>
+        <button onClick={() => onFilter("Shooter")}>Shooter</button>
+        <button onClick={() => onFilter("Casual")}>Casual</button>
+        <button onClick={() => onFilter("Simulation")}>Simulation</button>
+        <button onClick={() => onFilter("Arcade")}>Arcade</button>
+        <button onClick={() => onFilter("Platformer")}>Platformer</button>
+        <button onClick={() => onFilter("Sports")}>Sports</button>
+        <button onClick={() => onFilter("Fighting")}>Fighting</button>
         <button onClick={() => onFilter("All")}>All</button>
         <input className="search-bar" type="text" placeholder="Search.." />
       </div>
