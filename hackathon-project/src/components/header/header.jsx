9667091883bea60f1524494750c6bbc7e70721b6
Conner Adamsons <185359332+ConnerConner96@users.noreply.github.com>
2025-01-30 16:55:59 +0000

    added buttons onto header bar for filtering via gamemodes

--- hackathon-project/src/components/header/header.jsx
+++ hackathon-project/src/components/header/header.jsx
@@ -1,8 +1,19 @@
+import React from 'react';
+import './header.css';
 
 export default function Header() {
   return (
     <header className="header">
       <h1>CSC Gamecards Catalogue</h1>
+
+            {/* Genre Filter Buttons */}
+            <div className="filter-buttons">
+        <button onClick={() => onFilter("Action")}>Action</button>
+        <button onClick={() => onFilter("FPS")}>Action</button>
+        <button onClick={() => onFilter("RPG")}>RPG</button>
+        <button onClick={() => onFilter("Adventure")}>Adventure</button>
+        <button onClick={() => onFilter("All")}>All</button>
+      </div>
     </header>
   );
 }
